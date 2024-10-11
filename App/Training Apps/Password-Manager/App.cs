8ef44250b392989e54c0800946251f34fb3173e5
A7medEsam13 <aesam4168@gmail.com>
2024-10-12 02:54:44 +0300

    i have edited the password manger program and tommorrow i will continue editing and adding features

--- App/Training Apps/Password-Manager/App.cs
+++ App/Training Apps/Password-Manager/App.cs
@@ -11,8 +11,10 @@ namespace CSharpFundamentals.Training_Apps.Password_Manager
         private static readonly Dictionary<string, string> _PasswordEntries = new();
         public static void Run(string[] args)
         {
+            ReadPasswords();
             while (true)
             {
+                
                 Console.WriteLine("Please, select an option: ");
                 Console.WriteLine("1. List all passwords.");
                 Console.WriteLine("2. Add/Change password.");
@@ -24,61 +26,99 @@ namespace CSharpFundamentals.Training_Apps.Password_Manager
                 {
                     ListAllPasswords();
                 }
-                else if (selectedOption == "2")
-                {
-                    AddOrChangePassword();
+                    else if (selectedOption == "2") 
+                    {
+                        AddOrChangePassword();
+                    }
+                    else if (selectedOption == "3") 
+                    {
+                        GetPassword();
+                    }
+                    else
+                    {
+                        DeletePassword();
+                    }
+
+                    Console.WriteLine("--------------------------------------------------");
                 }
-                else if (selectedOption == "3")
+            }
+
+            private static void ListAllPasswords()
+            {
+                
+                foreach (var entry in _PasswordEntries)
                 {
-                    GetPassword();
+                    Console.WriteLine($"{entry.Key} = {entry.Value}");
+                }
+            }
+
+            private static void AddOrChangePassword()
+            {
+                Console.Write("Enter the Web site Name: ");
+                var key = Console.ReadLine();
+                Console.Write("Enter the Password : ");
+                var pass = Console.ReadLine();
+
+                if (_PasswordEntries.ContainsKey(key))
+                {
+                    _PasswordEntries[key] = pass;
                 }
                 else
                 {
-                    DeletePassword();
+                    _PasswordEntries.Add(key, pass);    
                 }
-
-                Console.WriteLine("--------------------------------------------------");
+                SavePasswords();
             }
-        }
 
-        private static void ListAllPasswords()
-        {
-            foreach (var entry in _PasswordEntries)
+            private static void GetPassword()
             {
-                Console.WriteLine($"{entry.Key} = {entry.Value}");
-            }
-        }
-
-        private static void AddOrChangePassword()
-        {
-            Console.Write("Enter the Web site Name: ");
-            var key = Console.ReadLine();
-            Console.Write("Enter the Password : ");
-            var pass = Console.ReadLine();
-
-            if (_PasswordEntries.ContainsKey(key))
-            {
-                _PasswordEntries[key] = pass;
-            }
+                Console.Write("Enter The Web site name: ");
+                var siteName = Console.ReadLine();
+            if(_PasswordEntries.ContainsKey(siteName))
+                Console.WriteLine($"Password of {siteName} = {_PasswordEntries[siteName]}");
             else
             {
-                _PasswordEntries.Add(key, pass);
+                Console.WriteLine("Password Of this site name doesn't exist");
             }
+            }
+
+            private static void DeletePassword()
+            {
+                Console.Write("Enter The name of web site: ");
+                var siteName = Console.ReadLine();
+                _PasswordEntries.Remove(siteName);
+                SavePasswords();
+                Console.WriteLine($"Password of {siteName} has been deleted :)");
+            }
+
+
+        private static void ReadPasswords()
+        {
+            if (File.Exists("Passwords.txt"))
+            {
+                var PasswordLines = File.ReadAllText("Passwords.txt");
+                foreach (var line in PasswordLines.Split(Environment.NewLine))
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        var EqualIndex = line.IndexOf('=');
+                        var AppName = line.Substring(0, EqualIndex);
+                        var password = line.Substring(EqualIndex + 1);
+                        _PasswordEntries.Add(AppName, password);
+                    }
+                }
+            }
+            
         }
 
-        private static void GetPassword()
+        private static void SavePasswords()
         {
-            Console.Write("Enter The Web site name: ");
-            var siteName = Console.ReadLine();
-            Console.Write(_PasswordEntries[siteName]);
-        }
-
-        private static void DeletePassword()
-        {
-            Console.Write("Enter The name of web site: ");
-            var siteName = Console.ReadLine();
-            _PasswordEntries.Remove(siteName);
-            Console.WriteLine($"Password of {siteName} has been deleted :)");
+            var sb = new StringBuilder();
+            foreach(var entry in _PasswordEntries)
+            {
+                sb.AppendLine($"{entry.Key} = {entry.Value}");
+            }
+            File.WriteAllText("Passwords.txt" , sb.ToString());
         }
     }
 }
